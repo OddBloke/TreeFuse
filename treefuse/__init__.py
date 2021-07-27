01f904b9826a49c6d10d6908bd2c1df84dbd979c
Dan Watkins <dwatkins@digitalocean.com>
2021-07-27 14:25:28 -0400

    treefuse/__init__.py: run black

--- treefuse/__init__.py
+++ treefuse/__init__.py
@@ -1,8 +1,8 @@
 """Top-level package for TreeFuse."""
 
 __author__ = """Daniel Watkins"""
-__email__ = 'daniel@daniel-watkins.co.uk'
-__version__ = '0.1.0'
+__email__ = "daniel@daniel-watkins.co.uk"
+__version__ = "0.1.0"
 
 from .treefuse import treefuse_main
 
