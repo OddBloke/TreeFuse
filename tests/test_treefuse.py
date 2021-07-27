6e7cb5e9bb88800023ad2136f86c779a8fb4d9b6
Dan Watkins <dwatkins@digitalocean.com>
2021-07-27 10:52:17 -0400

    tests: switch from tmpdir fixture to tmp_path

--- tests/test_treefuse.py
+++ tests/test_treefuse.py
@@ -16,14 +16,14 @@ from treefuse import treefuse_main
 
 
 @pytest.fixture
-def mount_tree(tmpdir):
-    """Provides a callable which mounts a given treelib.Tree in a tmpdir.
+def mount_tree(tmp_path):
+    """Provides a callable which mounts a given treelib.Tree in a tempdir.
 
     It handles mounting in a background process, waiting for the mount to
     appear in the filesystem, and unmounting the tmpdir during teardown.
 
-    This uses the `tmpdir` fixture: pytest will provide the same directory to
-    consuming tests which request the `tmpdir` fixture.
+    This uses the `tmp_path` fixture: pytest will provide the same directory to
+    consuming tests which request the `tmp_path` fixture.
     """
     process = None
 
@@ -32,7 +32,7 @@ def mount_tree(tmpdir):
         # Run treefuse_main in a separate process so we can continue test
         # execution in this one
         process = multiprocessing.Process(target=treefuse_main, args=(tree,))
-        with mock.patch("sys.argv", ["_test_", str(tmpdir)]):
+        with mock.patch("sys.argv", ["_test_", str(tmp_path)]):
             process.start()
         # As FUSE initialisation is happening in the background, we wait until
         # it's mounted before returning control to the test code.
@@ -40,7 +40,10 @@ def mount_tree(tmpdir):
         while attempts:
             # all=True to include FUSE filesystems
             partitions = psutil.disk_partitions(all=True)
-            if len([p for p in partitions if p.mountpoint == str(tmpdir)]) > 0:
+            if (
+                len([p for p in partitions if p.mountpoint == str(tmp_path)])
+                > 0
+            ):
                 # We're mounted!
                 break
             time.sleep(0.05)
@@ -52,7 +55,7 @@ def mount_tree(tmpdir):
         yield _mounter
     finally:
         if process is not None:
-            subprocess.check_call(["umount", str(tmpdir)])
+            subprocess.check_call(["umount", str(tmp_path)])
             process.join()
         else:
             warnings.warn(
@@ -60,7 +63,7 @@ def mount_tree(tmpdir):
             )
 
 
-def test_basic_tree(mount_tree, tmpdir):
+def test_basic_tree(mount_tree, tmp_path):
     """Test we can mount a basic tree structure."""
     tree = treelib.Tree()
     root = tree.create_node("root")
@@ -70,5 +73,7 @@ def test_basic_tree(mount_tree, tmpdir):
 
     mount_tree(tree)
 
-    assert tmpdir.join("rootchild").read() == "rootchild content"
-    assert tmpdir.join("dir1", "dirchild").read() == "dirchild content"
+    assert tmp_path.joinpath("rootchild").read_text() == "rootchild content"
+    assert (
+        tmp_path.joinpath("dir1", "dirchild").read_text() == "dirchild content"
+    )
